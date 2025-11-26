4aee0823911e056a9d2d3d3849e550b1abdebf70
Milololo-Zz <emileo.er@gmail.com>
2025-11-26 09:30:55 -0600

    Feat: Integracion Back-Front completada (API REST + CORS + React Fetch)

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,3 +129,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configuración CORS (Permitir todo para desarrollo)
+CORS_ALLOW_ALL_ORIGINS = True
